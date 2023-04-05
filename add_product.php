6c9f921ba67232b6b68f0e754cd8e729aaeb63f3
joris-landaret <joris.landaret@laplateforme.io>
2023-04-05 14:59:19 +0200

    add product, html : modifies category and sub category into a single and multiple choice drop down menu

--- add_product.php
+++ add_product.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ajouter un produit</title>
 </head>
+
 <body>
     <main>
         <section>
@@ -19,10 +21,27 @@
                 <input type="text" name="price">
                 <label for="">quantité</label>
                 <input type="text" name="quantity_product">
-                <label for="">catégorie</label>
-                <input type="text" name="catégory_product">
-                <label for="">sous-catégorie</label>
-                <input type="text" name="sub_category_product">
+                <label for="">Catégorie</label>
+                <select name="categorie" id="">
+                    <option value="Comics">Comics</option>
+                    <option value="Bande dessinée">Bande dessinée</option>
+                    <option value="Manga">Manga</option>
+                    <option value="Manhwa">Manhwa</option>
+                    <option value="Manhua">Manhua</option>
+                </select>
+                <label for="">Sous-catégorie</label>
+                <select name="categorie" id="" multiple>
+                    <option value="Action">Action</option>
+                    <option value="Fantaisie">Fantaisie</option>
+                    <option value="Isekai">Isekai</option>
+                    <option value="Drame">Drame</option>
+                    <option value="Psychologique">Psychologique</option>
+                    <option value="Comedie">Comedie</option>
+                    <option value="Policier">Policier</option>
+                    <option value="Science fiction">Science fiction</option>
+                    <option value="Aventure">Aventure</option>
+                    <option value="Mecha">Mecha</option>
+                </select>
                 <label for="">description</label>
                 <input type="textarea" name="descritpion_product">
                 <input type="submit" value="ajouter le produit">
@@ -30,4 +49,5 @@
         </section>
     </main>
 </body>
+
 </html>
