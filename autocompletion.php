2d3f69d5b22a1b4164b1d152117b30f43d4b6467
samuel-durand <114388132+samuel-durand@users.noreply.github.com>
2023-04-12 17:29:12 +0200

    udpate update js autocompletion

--- autocompletion.php
+++ autocompletion.php
@@ -1,41 +1,27 @@
 <?php 
-
-$pdo = new PDO('mysql:host=localhost;dbname=boutique', 'root', '');
-
-if (isset($_GET['q'])){
-    $query = $_GET['q'];
-
-    //requete
-
-
-// Connexion à la base de données
 $pdo = new PDO('mysql:host=localhost;dbname=boutique', 'root', '');
 
 if (isset($_GET['q'])){
     $query = $_GET['q'];
 
     // Requête SQL pour chercher les suggestions d'autocomplétion
-    $stmt = $pdo->prepare("SELECT id, name, price  FROM product WHERE name LIKE :query LIMIT 10");
+    $stmt = $pdo->prepare("SELECT id, name, price, id_category FROM product WHERE name LIKE :query LIMIT 10");
     $stmt->bindValue(':query', '%'.$query.'%');
     $stmt->execute();
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Transformation du tableau de résultats pour récupérer les noms, les prix et les identifiants de chaque produit
+    // Transformation du tableau de résultats pour récupérer les noms, les prix, les identifiants et les identifiants de catégorie de chaque produit
     $suggestions = array();
     foreach ($results as $result) {
       $id = $result['id'];
       $name = $result['name'];
       $price = $result['price'];
-      $suggestion = array('id' => $id, 'name' => $name, 'price' => $price);
+      $category_id = $result['id_category'];
+      $suggestion = array('id' => $id, 'name' => $name, 'price' => $price, 'category_id' => $category_id);
       array_push($suggestions, $suggestion);
     }
 
     // Affichage des suggestions au format JSON
     echo json_encode($suggestions);
 }
-
-}
-
-
-
 ?>
--- research.js
+++ research.js
@@ -1,23 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const form = document.querySelector('#mon-formulaire');
-    const search = form.querySelector('#recherche');
+    const search = document.querySelector('#recherche');
     const suggestions = document.querySelector('#suggestions');
   
-    form.addEventListener('submit', function(event) {
-      event.preventDefault();
-      const query = search.value.trim();
-      if (query.length < 0) {
-        alert('La requête de recherche doit contenir au moins 2 caractères.');
-        return;
-      }
-      const url = `element.php?q=${query}`;
-      window.location.href = url;
-    });
-  
     search.addEventListener('input', function() {
-      const query = search.value.trim();
+      const query = search.value;
   
-      if (query.length < 0) {
+      if (query.trim().length < 0) {
         suggestions.innerHTML = '';
         return;
       }
@@ -30,15 +18,11 @@ document.addEventListener('DOMContentLoaded', function() {
             const id = result.id;
             const name = result.name;
             const price = result.price;
-            const imageUrl = result.image_url;
+            const category_id = result.category_id;
             const element = document.createElement('div');
             const link = document.createElement('a');
-            const image = document.createElement('img');
             link.href = `product.php?id=${id}`;
-            image.src = imageUrl;
-            image.style.width = '50px'; // ajuster la taille de l'image
-            link.innerText = `${name} - ${price} €`;
-            element.appendChild(image);
+            link.innerText = `${name} - ${price} € (catégorie ${category_id})`;
             element.appendChild(link);
             suggestions.appendChild(element);
           });
