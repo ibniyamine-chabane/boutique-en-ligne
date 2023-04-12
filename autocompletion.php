ef0c905b69f13b8ee472cf33cd985281cd089633
samuel-durand <114388132+samuel-durand@users.noreply.github.com>
2023-04-12 11:30:16 +0200

    work

--- autocompletion.php
+++ autocompletion.php
@@ -8,14 +8,32 @@ if (isset($_GET['q'])){
     //requete
 
 
-  // Requête SQL pour chercher les suggestions d'autocomplétion
-  $stmt = $pdo->prepare("SELECT name FROM product WHERE name LIKE :query LIMIT 10");
-  $stmt->bindValue(':query', '%'.$query.'%');
-  $stmt->execute();
-  $results = $stmt->fetchAll(PDO::FETCH_COLUMN);
+// Connexion à la base de données
+$pdo = new PDO('mysql:host=localhost;dbname=boutique', 'root', '');
 
-  // Affichage des suggestions au format JSON
-  echo json_encode($results);
+if (isset($_GET['q'])){
+    $query = $_GET['q'];
+
+    // Requête SQL pour chercher les suggestions d'autocomplétion
+    $stmt = $pdo->prepare("SELECT id, name, price FROM product WHERE name LIKE :query LIMIT 10");
+    $stmt->bindValue(':query', '%'.$query.'%');
+    $stmt->execute();
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Transformation du tableau de résultats pour récupérer les noms, les prix et les identifiants de chaque produit
+    $suggestions = array();
+    foreach ($results as $result) {
+      $id = $result['id'];
+      $name = $result['name'];
+      $price = $result['price'];
+      $suggestion = array('id' => $id, 'name' => $name, 'price' => $price);
+      array_push($suggestions, $suggestion);
+    }
+
+    // Affichage des suggestions au format JSON
+    echo json_encode($suggestions);
+}
+
 }
 
 
--- research.js
+++ research.js
@@ -1,30 +1,38 @@
-  document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     const search = document.querySelector('#recherche');
     const suggestions = document.querySelector('#suggestions');
-
-    search.addEventListener('keyup', function() {
+  
+    search.addEventListener('input', function() {
       const query = search.value;
-
-      if (query !== '') {
-        fetch(`autocompletion.php?q=${query}`)
-          .then(response => response.json())
-          .then(data => {
-            suggestions.innerHTML = '';
-            data.forEach(suggestion => {
-              const div = document.createElement('div');
-              div.textContent = suggestion;
-              div.addEventListener('click', function() {
-                search.value = suggestion;
-                suggestions.innerHTML = '';
-              });
-              suggestions.appendChild(div);
-            });
-          });
-      } else {
+  
+      if (query.trim().length < 2) {
         suggestions.innerHTML = '';
+        return;
       }
+  
+      fetch(`autocompletion.php?q=${query}`)
+        .then(response => response.json())
+        .then(results => {
+          suggestions.innerHTML = '';
+          results.forEach(result => {
+            const id = result.id;
+            const name = result.name;
+            const price = result.price;
+            const element = document.createElement('div');
+            const link = document.createElement('a');
+            link.href = `product.php?id=${id}`;
+            link.innerText = `${name} - ${price} €`;
+            element.appendChild(link);
+            suggestions.appendChild(element);
+          });
+        })
+        .catch(error => {
+          console.error(error);
+          suggestions.innerHTML = 'Erreur de chargement des résultats.';
+        });
     });
   });
+  
 
 
 
