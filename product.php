4ea13b0d09f57a4108c6e376a8837b4669e295b4
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2023-04-24 19:17:35 +0200

    css of the page product.php started and not done yet must do the css for the page profil

--- product.php
+++ product.php
@@ -34,6 +34,7 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/css/style.css">
     <title>Produit</title>
 </head>
 <body>
@@ -42,32 +43,39 @@ if (isset($_POST['submit'])) {
         <div></div>
         <section>
             <?php foreach ($productDatabase as $product) : ?>
-            <h2><?= $product['name'] ?></h2>
-            <div>
-                <img src="src/upload/<?= $product['image'] ?>" alt="">
-            </div>
-            <article>
-                <h3><?= $product['description'] ?></h3>
-                <aside>
-                    <p></p>
-                </aside>
-            </article>
-            <div>
-                <form action="" method="post">
-                    <label for="product_quantity">quantité :</label>
-                    <input type="hidden" name="" value=""><!-- ici la value sera l'id de l'user inscrit -->
-                    <input type="hidden" value=""><!-- ici la value sera l'id du produit -->
-                    <input type="number" name="quantity" maxlength="4" size="3" min="1" max="<?= $productDatabase[0]['quantity'] ?>"><!-- le max sera la quantité disponible pour ce produit, ce champ a mettre en width: 39px ne pas oublier de transformer le number en string-->
-                    <?php if ($productDatabase[0]['quantity'] == 0):?>
-                    <p>rupture de stock</p>
-                    <?php else : ?>
-                    <p><?= $productDatabase[0]['quantity'] ?> en stock</p>
-                    <?php endif; ?>
-                    <input type="submit" name="submit" value="ajouté au panier"> 
-                </form>
-            </div>
+                <h2><?= $product['name'] ?></h2>
+                <div class="container-product-page">
+                    <div class="container-image">
+                        <div class="image-box">
+                            <img src="src/upload/<?= $product['image'] ?>" alt="">
+                        </div>
+                    </div>
+                    <div class="right-side">
+                        <article>
+                            <h3>description</h3>
+                            <aside>
+                                <p><?= $product['description'] ?></p>
+                            </aside>
+                        </article>
+                        <div class="container-form-quantity">
+                            <form action="" method="post">
+                                <label for="product_quantity">quantité :</label>
+                                <input type="hidden" name="" value=""><!-- ici la value sera l'id de l'user inscrit -->
+                                <input type="hidden" value=""><!-- ici la value sera l'id du produit -->
+                                <input type="number" name="quantity" maxlength="4" size="3" min="1" max="<?= $productDatabase[0]['quantity'] ?>"><!-- le max sera la quantité disponible pour ce produit, ce champ a mettre en width: 39px ne pas oublier de transformer le number en string-->
+                                <?php if ($productDatabase[0]['quantity'] == 0):?>
+                                <p>rupture de stock</p>
+                                <?php else : ?>
+                                <p><?= $productDatabase[0]['quantity'] ?> en stock</p>
+                                <?php endif; ?>
+                                <input type="submit" name="submit" value="ajouté au panier" class="green-button"> 
+                            </form>
+                        </div>
+                    </div>
+                </div>
         </section>
         <?php endforeach; ?>
     </main>
+    <?php require_once("footer.php"); ?>
 </body>
 </html>
