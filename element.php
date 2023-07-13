b27ee25ccf009e2e7127c63617d2bef2ea71e621
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2023-07-13 13:00:04 +0200

    container added on the page

--- element.php
+++ element.php
@@ -12,14 +12,17 @@ include('src/class/users.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="src/css/style.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script defer src="result.js"></script>
     <title>Résultats de la recherche</title>
 </head>
 <body>
     <?php include('header.php') ?>
-    <h1>Résultats de la recherche pour "<span id="search-query"></span>" :</h1>
-    <div id="products"></div>
-
-    <script src="result.js"></script>
+    <main>
+        <div class="container-result">
+            <h1>Résultats de la recherche pour "<span id="search-query"></span>" :</h1>
+            <div id="products"></div>
+        </div>
+    </main>
 </body>
 </html>
 
